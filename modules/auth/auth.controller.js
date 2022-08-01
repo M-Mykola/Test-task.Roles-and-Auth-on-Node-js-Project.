6c1cfe1c59873b18b73f9534bf5472794915ca0d
Mykola Matiko <matiko.mikola@gmail.com>
2022-08-02 01:45:33 +0300

    Roles has been tested.'Get Users List' functional works good with all requirements!

--- modules/auth/auth.controller.js
+++ modules/auth/auth.controller.js
@@ -18,15 +18,15 @@ class AuthController {
       const user = new User({
         userName: userName,
         password: hashPassword,
-        role: roles.USER,
+        role: roles.ADMIN,
       });
       await user.save();
       return res.status(200).json({ message: "User has been registered." });
     } catch (error) {
-      console.log(error);
-      res.status(400).json({ message: "Registatration error." });
+      return res.status(400).json({ message: "Registatration error." });
     }
   }
+
   async login(req, res) {
     try {
       const user = await User.findOne({ userName: req.body.userName });
@@ -42,35 +42,35 @@ class AuthController {
       }
       const secret = process.env.SECRET_KEY;
       const token = jwt.sign(
-        { user_id: user._id, userName: req.body.userName },
+        { user_id: user._id, userName: user.userName, role: user.role },
         secret,
         {
           expiresIn: "1d",
         }
       );
-      console.log(token);
-      res
+      return res
         .status(200)
         .json({ message: `Welcome ${user.userName}`, user, token });
     } catch (error) {
-      console.log(error);
-      res.status(400).json({ message: "Login error." });
-    }
-  }
-  async verifyToken(req, res, next) {
-    const token =
-      req.body.token || req.query.token || req.headers["access-token"];
-    if (!token) {
-      res.status(403).send({ message: "Token is required" });
-    }
-    try {
-      const secret = process.env.SECRET_KEY;
-      const decoded = jwt.verify(token, secret);
-      req.user = decoded;
-    } catch (error) {
-      res.status(401).send({ message: "Invalid token" });
+      return res.status(400).json({ message: "Login error." });
     }
   }
 
+  async verifyToken(req, res, next) {
+    const token =
+      req.body.token || req.query.token || req.headers["x-access-token"];
+    if (!token) {
+      return res.status(403).send({ message: "Token is required" });
+    }
+    try {
+      const secret = process.env.SECRET_KEY;
+      const decoded = jwt.verify(token, secret, roles);
+      req.user = decoded;
+    } catch (error) {
+      return res.status(401).send({ message: "Invalid token" });
+    }
+    return next();
+  }
 }
+
 module.exports = new AuthController();
